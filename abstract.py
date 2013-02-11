1f52bfec43f3f71e582987e0390cff37403d6aa9
George Cockshull <george.cockshull@gmail.com>
2013-02-11 01:31:36 +0000

    Added command line options, latex document skeletons with substitution. DLD now defines two functions, src preparse(src) and postparse(ast) allowing the code to be pre-filtered and preamble and settings/options adjusted. Created a new sample test2.dld, implementing a graph with matrix layout and connections using chains. Whole system has been made a lot more flexible. Input source file can be specified on command line and full LaTeX document output can be enabled with the --latex flag. TODO: chain branching, smart layout functions, better error messages

--- abstract.py
+++ abstract.py
@@ -8,30 +8,68 @@ class Graph(object):
     def add(self, obj):
         if isinstance(obj, GraphNode):
             self.nodes.append(obj)
+            obj.num = len(self.nodes)-1
         elif isinstance(obj, GraphEdge):
             self.edges.append(obj)
 
-    def emit(self):
-        print r"\begin{tikzpicture}"
-        print r"\matrix[column sep=0.5cm, row sep=0.5cm] {"
-        for n in self.nodes:
+    def emit_node_matrix(self, numperrow=1, sep='5mm'):
+        print r"\matrix[column sep="+sep+r", row sep="+sep+r"] {"
+        for num, n in enumerate(self.nodes):
             n.emit()
+            if (num+1) % numperrow == 0 or num == len(self.nodes)-1:
+                print r"\\"
+            else:
+                print r"&"
+        print r"};"
+
+    def emit_chain(self):
+        print r"{ [start chain] ";
+        nprev = None
+        for num, n in enumerate(self.nodes):
+            fromme = self.edges_from(num)
+            tome = self.edges_to(num)
+            if not tome:
+                print r"\chainin (n"+str(num)+r");"
+            else:
+                for s in tome:
+                    if s.src == nprev:
+                        print r"\chainin (n"+str(num)+r") [join=by "+s.cls+r"];"
+
+            #TODO: branching based on edges from this one (that arent the next)
+
+            nprev = num
+        print r"};"
+
+    def edges_from(self, node):
+        res = []
         for e in self.edges:
-            e.emit()
-        print r"}; \end{tikzpicture}"
+            if e.src == node:
+                res.append(e)
+        return res
+
+    def edges_to(self, node):
+        res = []
+        for e in self.edges:
+            if e.dest == node:
+                res.append(e)
+        return res
+
+    def node_search(self):
+        return len(self.nodes)-1
 
 
 class GraphNode():
 
-    def __init__(self, name):
+    def __init__(self, name, cls='draw', num=-1):
         self.name = name
+        self.cls = cls
+        self.num = num
 
     def emit(self):
-        print r"\node[circle] {"+self.name+r"};"
+        print r"\node["+self.cls+r"] (n"+str(self.num)+r") {"+self.name+r"};",
 
 class GraphEdge():
-    
-    def __init__(self, name):
-        pass
-    def emit(self):
-        print r"\edge ..."
+    def __init__(self, src, dest, cls='->,>=latex'):
+        self.src = src
+        self.dest = dest
+        self.cls = cls
--- dml.py
+++ dml.py
@@ -1,11 +1,89 @@
 from plyplus import Grammar
-import pydot
+import imp
+import argparse
+
+docpreamble = r"""
+\documentclass[%_document_class_options_%]{%_document_class_%}
+\usepackage[%_tikz_package_options_%]{tikz}
+\usetikzlibrary{%_tikz_libraries_%}
+%_additional_preamble_%
+\begin{document}
+"""
+
+tikzheader = r"""
+\begin{tikzpicture}[%_tikzpicture_env_options_%]
+"""
+
+tikzfooter = r"""
+\end{tikzpicture}
+"""
+
+docpostamble = r"""
+\end{document}
+"""
 
 if __name__ == '__main__':
-    fname = "test.dld"
-    s = raw_input("> ")
-    (grammar, py) = file(fname).read().split("###")
+
+    argparser = argparse.ArgumentParser(description='Diagram Markup Language Processor')
+    argparser.add_argument('deffile', metavar='definition file', action='store', type=argparse.FileType('r'))
+    argparser.add_argument('-s', '--src', action='store', dest='sourcefile', type=argparse.FileType('r'))
+    argparser.add_argument('--latex', help='output full latex document with preamble', action='store_true')
+    argparser.add_argument('--renderast', help='also render AST to ast.png', action='store_true')
+    argparser.add_argument('--options', help='additional tikzpicture environment options', action='store', dest='tikzpictureoptions', type=str)
+    arg = argparser.parse_args()
+
+
+    if(arg.sourcefile==None):
+        s = ''
+        while 1:
+            try: l = raw_input()
+            except EOFError: break
+            s += l
+    else:
+        s = arg.sourcefile.read()
+        
+    (grammar, py) = arg.deffile.read().split("###")
     g = Grammar(grammar);
+
+    #load python code into its own module
+    m = imp.new_module("tempdmlmodule")
+    exec py in m.__dict__
+
+    #default substitutions
+    document_class = 'minimal'
+    document_class_options = ''
+    tikz_package_options = ''
+    tikzpicture_env_options = '' if not arg.tikzpictureoptions else arg.tikzpictureoptions
+    additional_preamble = ''
+    tikz_libraries = []
+
+    s = m.preparse(s)
+
+    #make substitutions
+    docpreamble = docpreamble.replace('%_document_class_%', document_class)
+    docpreamble = docpreamble.replace('%_document_class_options_%', document_class_options)
+    docpreamble = docpreamble.replace('%_tikz_package_options_%', tikz_package_options)
+    docpreamble = docpreamble.replace('%_tikz_libraries_%', ','.join(tikz_libraries))
+    docpreamble = docpreamble.replace('%_additional_preamble_%', additional_preamble)
+    tikzheader = tikzheader.replace('%_tikzpicture_env_options_%', tikzpicture_env_options)
+    
+    
+    if arg.latex:
+        print docpreamble
+    print tikzheader
+
     ast = g.parse(s)
-    ast.to_png_with_pydot("ast.png");
-    exec py
+    m.postparse(ast)
+
+    print tikzfooter
+    if arg.latex:
+        print docpostamble
+        
+
+    if arg.renderast:
+        import pydot
+        ast.to_png_with_pydot("ast.png");
+
+
+
+    
